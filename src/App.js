49061d53b15b59159e182d76c8399a059ebb9f26
Mark Pratt <32585200+SaltyPineapple@users.noreply.github.com>
2022-10-23 09:38:47 -0700

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from "./components/header.jsx"
+import Separator from "./components/separator.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <Separator></Separator>
+    </div>
+  );
+}
+
+export default App;
