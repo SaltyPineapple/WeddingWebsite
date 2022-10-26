6add7a1661a7ca1580b3bb1ac811cd19ff18ff85
Mark <mjpratt31@gmail.com>
2022-10-25 23:52:48 -0700

    update links, start info component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components//header/header";
+import Info from "./components/info/info";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Info></Info>
     </div>
   );
 }
---
+++ src/components/info/info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./info.css"
+
+const Info = () => {
+    return (
+        <div className="info">
+
+        </div>
+    );
+}
+
+export default Info;
