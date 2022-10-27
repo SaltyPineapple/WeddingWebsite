ad8aa718203e7d6b39ea5e5c5aa867453261ffc2
Mark <mjpratt31@gmail.com>
2022-10-27 00:55:05 -0700

    Add picture to info page

--- src/components/info/info.jsx
+++ src/components/info/info.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import "./info.css"
+import picture from "../../images/locust-kiss.jpg"
 
 const Info = () => {
     return (
         <div className="info">
+            <div className="picture">
+                {/* <img src={picture} alt="" className="pic"/> */}
+            </div>
 
+            <div className="separator"></div>
+            
+            <div className="information">
+                <h2>Ceremony</h2>
+                <h3>4:00 PM</h3>
+                <p>Leavenworth, WA</p>
+            </div>
         </div>
     );
 }
