640c490832ab07b4a0bad2ed90e31b5fdc4b2cc3
Mark <mjpratt31@gmail.com>
2022-10-28 00:16:40 -0700

    More image work

--- src/components/info/info.jsx
+++ src/components/info/info.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import "./info.css"
-import picture from "../../images/locust-kiss.jpg"
 
 const Info = () => {
     return (
         <div className="info">
             <div className="picture">
-                {/* <img src={picture} alt="" className="pic"/> */}
             </div>
 
-            <div className="separator"></div>
+            {/* <div className="separator"></div> */}
             
             <div className="information">
                 <h2>Ceremony</h2>
