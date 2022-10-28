e4e071aedb6afa2e1489c88daf4ce5443c72ad06
Mark <mjpratt31@gmail.com>
2022-10-28 00:34:30 -0700

    Add RSVP button

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -17,14 +17,19 @@ const Header = props => {
                 <div className={hasScrolledDown > 0 ? "container" : "containerTransparent"}>
                     <h1 className="headerName">Mark & Brianne</h1>
                     <div className={hasScrolledDown > 0 ? "nav" : "navNoDisplay"}>
-                        <a href="#location" className="link">Location</a> 
+                        <a href="#" className="link">Location & Stay</a> 
                         <a href="#" className="link">About</a> 
                         <a href="#" className="link">Registry</a> 
                         <a href="#" className="link">Registry</a> 
                         <a href="#" className="link">Important Info</a> 
                         <a href="#" className="link">Contact</a> 
                     </div>
+
                 </div>
+
+                <a href="#" className={hasScrolledDown > 0 ? "rsvp" : "rsvpTransparent"}>
+                    RSVP
+                </a>
 
                 <div className="heroText">
                     <h1>We're Getting Married!</h1>
