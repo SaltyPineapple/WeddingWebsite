e05279c96887c9577f08c2dde501f63d8e7518dc
Mark <mjpratt31@gmail.com>
2022-11-03 01:30:51 -0700

    Begin location info separator component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components//header/header";
 import Info from "./components/info/info";
+import Location from "./components/location/location";
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
+      <Header></Header>      
       <Info></Info>
+      <Location></Location>
     </div>
   );
 }
---
+++ src/components/location/location.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./location.css"
+
+const location = () => {
+
+    return(
+        <div className="location">
+            <div className="line"></div>
+            <button className="location-button">
+                View Location
+            </button>
+            <div className="line"></div>
+        </div>
+    );
+
+}
+
+export default location;
